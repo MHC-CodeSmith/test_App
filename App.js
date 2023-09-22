89ec9cbcd2571ecc55d071bde2b2302ace589f9f
Matheus Hipólito Carvalho <carvalho.matheush@gmail.com>
2023-09-22 09:43:37 -0300

    Update line endings and additional changes

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import TcpSocket from 'react-native-tcp-socket';
 
-export default function App() {
+const App = () => {
+  const [isConnected, setIsConnected] = useState(false);
+  const clientRef = useRef(null);
+
+  // Função para conectar ao servidor ESP32
+  const connectToESP32 = () => {
+    clientRef.current = TcpSocket.createConnection(
+      { host: '192.168.15.19', port: 80 },
+      () => {
+        console.log('Conectado ao ESP32');
+        setIsConnected(true);
+      }
+    );
+
+    clientRef.current.on('close', () => {
+      console.log('Desconectado do ESP32');
+      setIsConnected(false);
+    });
+
+    clientRef.current.on('error', (error) => {
+      console.error('Erro de conexão:', error);
+      setIsConnected(false);
+    });
+  };
+
+  // Função para enviar "0" para o ESP32
+  const sendZero = () => {
+    if (isConnected && clientRef.current) {
+      if (clientRef.current.connecting) {
+        console.log('Aguardando conexão completa...');
+      } else {
+        clientRef.current.write('0');
+      }
+    } else {
+      console.error('Não conectado ao ESP32');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.statusText}>
+        {isConnected ? 'Conectado ao ESP32' : 'Não conectado'}
+      </Text>
+      <Button title="Conectar ao ESP32" onPress={connectToESP32} />
+      <Button title="Enviar 0" onPress={sendZero} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  statusText: {
+    fontSize: 18,
+    marginBottom: 20,
   },
 });
+
+export default App;
